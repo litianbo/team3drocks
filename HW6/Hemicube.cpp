39398a41d66e2b0f861076020085e5b43204410c
litianbo <tianboli@usc.edu>
2014-12-04 03:36:53 -0800

    fixed minors

--- HW6/Hemicube.cpp
+++ HW6/Hemicube.cpp
@@ -25,14 +25,13 @@ void Hemicube::normalize(Position3 &a){
 }
 void Hemicube::setHemicube(Plane plane, int iteration){
 	iterations = iteration;
-	Plane base = plane;
 	for (int i = 0; i < 4; i++){
-		position[i] = base.position[i];
+		position[i] = plane.position[i];
 	}
 	//need nomral to cacalulate front view plane.
 	Position3 a = position[1] - position[0];
 	Position3 b = position[2] - position[0];
-	crossProduct(a, b, normal);
+	crossProduct(b, a, normal);
 	normalize(normal);
 	//construct front view vertexes
 	//find the center point using interpolation
@@ -49,46 +48,37 @@ void Hemicube::setHemicube(Plane plane, int iteration){
 	//find center in front plane
 	centerFront = center + normal;
 	//find the front plane vertexes
-	a = position[0] - position[3];
-	b = position[0] - position[1];
+	a = position[3] - position[0];
+	b = position[1] - position[0];
 	normalize(a);
 	normalize(b);
-	frontPosition[0] = centerFront + a + b;
-	frontPosition[1] = centerFront + a - b;
-	frontPosition[2] = centerFront - a - b;
-	frontPosition[3] = centerFront - a + b;
-	//extend the plane by 1 all around (top,bottom, left, right)
-	extendedPosition[0] = frontPosition[0] + a + b;
-	extendedPosition[1] = frontPosition[1] + a - b;
-	extendedPosition[2] = frontPosition[2] - a - b;
-	extendedPosition[3] = frontPosition[3] - a + b;
-	extendedPlane.setPlane(extendedPosition[0], extendedPosition[1], extendedPosition[2], extendedPosition[3]);
+	frontPosition[0] = centerFront - a - b;
+	frontPosition[1] = centerFront - a + b;
+	frontPosition[2] = centerFront + a + b;
+	frontPosition[3] = centerFront + a - b;
 	frontPlane.setPlane(frontPosition[0], frontPosition[1], frontPosition[2], frontPosition[3]);
 
 	//divide plane into many patches and put them into a list.
 	frontPatches = subdivide(frontPlane, iterations);
-	//need width and height to calculate patches in other sides
-	width = frontPatches[0].position[1] - frontPatches[0].position[0];
-	height = frontPatches[0].position[2] - frontPatches[0].position[1];
 	std::vector<Plane> tempLeftPatches1, tempRightPatches1, tempTopPatches1, tempBottomPatches1, 
 		tempLeftPatches2, tempRightPatches2, tempTopPatches2, tempBottomPatches2;
-	normalRight = b;
-	normalLeft.pos[0] = -b.pos[0];
-	normalLeft.pos[1] = -b.pos[1];
-	normalLeft.pos[2] = -b.pos[2];
+	normalLeft = b;
+	normalRight.pos[0] = -b.pos[0];
+	normalRight.pos[1] = -b.pos[1];
+	normalRight.pos[2] = -b.pos[2];
 	normalBottom = a;
 	normalTop.pos[0] = -a.pos[0];
 	normalTop.pos[1] = -a.pos[1];
 	normalTop.pos[2] = -a.pos[2];
 	//divide the side plane by 2 equal square
-	leftPlane1.setPlane(position[0], position[0] + normalTop, position[0] + normalTop - normal, position[0] - normal);
-	leftPlane2.setPlane(position[0] + normalTop, position[3], position[3] - normal, position[0] + normalTop - normal);
-	bottomPlane1.setPlane(position[3], position[3] + normalLeft, position[3] + normalLeft - normal, position[3] - normal);
-	bottomPlane2.setPlane(position[3] + normalLeft, position[2], position[2] - normal, position[3] + normalLeft - normal);
-	rightPlane1.setPlane(position[2], position[2] + normalBottom, position[2] + normalBottom - normal, position[2] - normal);
-	rightPlane2.setPlane(position[2] + normalBottom, position[1], position[1] - normal, position[2] + normalBottom - normal);
-	topPlane1.setPlane(position[1], position[1] + normalRight, position[1] + normalRight - normal, position[1] - normal);
-	topPlane2.setPlane(position[1] + normalRight, position[0], position[0] - normal, position[1] + normalRight - normal);
+	leftPlane1.setPlane(position[3] - normal, position[3] - normalBottom - normal, position[3] - normalBottom, position[3]);
+	leftPlane2.setPlane(position[0] - normal + normalBottom, position[0] - normal, position[0], position[0] + normalBottom);
+	bottomPlane1.setPlane(position[0] - normal, position[0] - normal + normalLeft, position[0] + normalLeft, position[0]);
+	bottomPlane2.setPlane(position[0] - normal + normalLeft, position[1] - normal, position[1], position[0] + normalLeft);
+	rightPlane1.setPlane(position[1] - normal, position[1] - normal + normalBottom, position[1] + normalBottom, position[1]);
+	rightPlane2.setPlane(position[1] - normal + normalBottom, position[2] - normal, position[2], position[1] + normalBottom);
+	topPlane1.setPlane(position[2] - normal, position[2] - normal - normalLeft, position[2] - normalLeft, position[2]);
+	topPlane2.setPlane(position[2] - normalLeft - normal, position[3] - normal, position[3],position[2] - normalLeft);
 	leftPatches1 = subdivide(leftPlane1, iterations / 2);
 	leftPatches2 = subdivide(leftPlane2, iterations / 2);
 	rightPatches1 = subdivide(rightPlane1, iterations / 2);
